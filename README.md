bb51c820b51c6bff43b6ee5522eb0543f595b569
Matt Copperwaite <matt@copperwaite.net>
2021-08-02 15:33:15 +0100

    root commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# html5-lo.fi
-An open source web-based embedded radio player
--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  entry: './src/js/index.js',
-  output: {
-    path: __dirname,
-    filename: 'main.js'
-  }
-};
