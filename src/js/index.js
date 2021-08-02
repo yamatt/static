88879d9066df9be161d91df96d428f295fe23113
Matt Copperwaite <matt@copperwaite.net>
2021-08-02 15:31:55 +0100

    root commit

---
+++ src/js/index.js
@@ -0,0 +1,20 @@
+const bgVideo = document.getElementById("bg-video");
+const player  = document.getElementById("player");
+
+const video_urls = new URL("backgrounds.json")
+
+fetch(video_urls)
+  .then(function(response) {
+    if (!response.ok) {
+    throw new Error("HTTP error, status = " + response.status);
+    }
+    return response.json();
+  })
+  .then(function(video_urls) {
+    var stream = video_urls[Math.floor(Math.random() * video_urls.length)];
+    bgVideo.setAttribute("src", stream['url']);
+    bgVideo.playbackRate = 0.5;
+  })
+  .catch(function(error) {
+    console.log(error.message);
+  });
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    path: __dirname,
+    filename: 'main.js'
+  }
+};
