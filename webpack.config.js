f8827bbc7b95894d7673590084e236ce91e13c92
Matt Copperwaite <matt@copperwaite.net>
2021-08-05 15:13:53 +0100

    trying out minified html

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlMinimizerPlugin = require("html-minimizer-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   devtool: "source-map",
@@ -7,5 +9,31 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     sourceMapFilename: "./main.js.map",
     filename: 'main.js'
-  }
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.html$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          context: __dirname,
+          from: "./src/html/*.html",
+        },
+      ],
+    }),
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
+      // `...`
+      new HtmlMinimizerPlugin(),
+    ],
+  },
 };
