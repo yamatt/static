05cd27991809ec5b384eef08b6006dc58e864724
Matt Copperwaite <matt@copperwaite.net>
2021-08-06 13:16:14 +0100

    include text nodes

--- src/js/info.js
+++ src/js/info.js
@@ -26,16 +26,16 @@ export default class Info {
 
     update_background(background) {
       let content = background.title;
-      if (this.background_el.children.length > 0) {
-        this.background_el.children[0].remove()
+      if (this.background_el.childNodes.length > 0) {
+        this.background_el.childNodes[0].remove()
       }
       this.background_el.appendChild(document.createTextNode(content))
     }
 
     update_stream(stream) {
       let content = stream.title;
-      if (this.stream_el.children.length > 0) {
-        this.stream_el.children[0].remove()
+      if (this.stream_el.childNodes.length > 0) {
+        this.stream_el.childNodes[0].remove()
       }
       this.stream_el.appendChild(document.createTextNode(content))
     }
