1bf1df0e330c0a428aa2a936c885d2d8cdd5c074
Matt Copperwaite <matt@copperwaite.net>
2021-08-06 09:07:31 +0100

    removing html minimizer because it's still broken

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,4 @@
 const path = require('path');
-const HtmlMinimizerPlugin = require("html-minimizer-webpack-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   devtool: "source-map",
@@ -9,29 +7,5 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     sourceMapFilename: "./main.js.map",
     filename: 'main.js'
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.html$/i,
-        type: "asset/resource",
-      },
-    ],
-  },
-  plugins: [
-    new CopyPlugin({
-      patterns: [
-        {
-          context: __dirname,
-          from: "./src/html/*.html",
-        },
-      ],
-    }),
-  ],
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new HtmlMinimizerPlugin(),
-    ],
-  },
+  }
 };
