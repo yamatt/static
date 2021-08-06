0e02a38b140b7fc37fd82b0456f20b2b8402e7fc
Matt Copperwaite <matt@copperwaite.net>
2021-08-06 09:49:54 +0100

    then auto play

--- src/js/player.js
+++ src/js/player.js
@@ -76,5 +76,6 @@ export default class Player {
     this.get_json(this.STREAMS_URL, this.handle_streams)
       .then(this.handle_streams.bind(this))
       .then(this.change_stream.bind(this))
+      .then(this.play.bind(this))
   }
 }
